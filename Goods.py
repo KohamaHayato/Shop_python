5dfd7effb0fdff3fc836f50622492ff0e6181028
KohamaHayato <007.hamako@gmail.com>
2020-07-14 20:56:24 +0900

    python

---
+++ Goods.py
@@ -0,0 +1,23 @@
+class Goods:
+    def __init__(self,goods_name,price):
+        self.__goods_name = goods_name                                  
+        self.__price = price
+
+    def print_goods(self):
+        print('?(Goods) ' + self.__goods_name + ' ' + str(self.__price) + '円')
+
+    def set_goods_name(self,goods_name):
+        self.__goods_name = goods_name
+    
+    def get_goods_name(self):
+        return self.__goods_name
+    
+    def set_price(self,price):
+        self.__price = price
+    
+    def get_price(self):
+        return self.__price
+    
+    goods_name = property(set_goods_name,get_goods_name)
+
+    price = property(set_price,get_price)
---
+++ Main.py
@@ -0,0 +1,6 @@
+from Shop import *
+
+tokyo = Shop('東京店','03-1234-5678')
+
+tokyo.create_goods("海洋深層水",1200)
+tokyo.print_shop()
