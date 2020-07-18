a0c847d547bb09e3c1e7f4ef2af89cf35d765204
KohamaHayato <007.hamako@gmail.com>
2020-07-18 20:54:43 +0900

    S1-3まで完了

--- Main.py
+++ Main.py
@@ -1,13 +1,11 @@
 from Shop import *
 from Customer import *
-from Goods import *
-from ShoppingBag import *
 
 tokyo = Shop('東京店','03-1234-5678')
-goods = Goods('海洋深層水',1200)
-shopping_bag = ShoppingBag(1000)
+tokyo.create_goods('海洋深層水',1200)
 yamada = Customer('山田')
+yamada.create_shopping_bag(1000)
 
-tokyo.print_shop(goods)
-yamada.print_customer(shopping_bag)
+yamada.print_customer()
 yamada.query_shop(tokyo)
+
--- ShoppingBag.py
+++ ShoppingBag.py
@@ -1,14 +1,18 @@
+from Goods import *
+
 class ShoppingBag:
     def __init__(self,money):
         self.money = money
     
-    def print_shopping_bag(self,goods):
+    def print_shopping_bag(self):
+        goods = Goods()
         print('　(ShoppingBag) 限度額' + str(self.money) + '円')
-        if goods.get_goods_name() == None:
-            print('商品は入っていません。')
+        if goods.get_goods_name() == 'なし':
+            print('　(ShoppingBag) 商品は入っていません。')
         else :
             goods.print_goods()
 
+    '''
     def set_money(self,money):
         self.money = money
     
@@ -16,3 +20,4 @@ class ShoppingBag:
         return self.money
     
     money = property(set_money,get_money)
+    '''
