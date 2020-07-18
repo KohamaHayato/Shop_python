bb94fdd2c14b2136e16a1479f6d072303706e4d2
KohamaHayato <007.hamako@gmail.com>
2020-07-18 22:36:15 +0900

    ShoppingBagクラスでのGoodsクラスの初期化でできない

--- Goods.py
+++ Goods.py
@@ -18,6 +18,14 @@ class Goods:
     def get_price(self):
         return self.__price
     
+    def set_goods(self,goods):
+        self.__goods = goods
+    
+    def get_goods(self):
+        return self.__goods
+    
     goods_name = property(set_goods_name,get_goods_name)
 
     price = property(set_price,get_price)
+
+    goods = property(set_goods,get_goods)
--- Shop.py
+++ Shop.py
@@ -1,31 +1,42 @@
 from Goods import *
+from ShoppingBag import *
 
 class Shop:
     def __init__(self,shop_name,tel_no):
-        self.shop_name = shop_name
-        self.tel_no = tel_no
+        self.__shop_name = shop_name
+        self.__tel_no = tel_no
 
     def create_goods(self,goods_name,price):
         self.goods = Goods(goods_name,price)
 
     def print_shop(self):
-        print('(Shop) ' + self.shop_name + 'TEL:' + self.tel_no)
+        print('(Shop) ' + self.__shop_name + 'TEL:' + self.__tel_no)
         self.goods.print_goods()
+    
+    def sale_goods(self,goods_name,shopping_bag):
+        if(self.goods.get_goods_name() == goods_name):
+            if(shopping_bag.get_money() > self.goods.get_price()):
+                charge = shopping_bag.get_money() - self.goods.get_price()
+                shopping_bag.set_money(charge)
+                self.goods.set_goods(self.goods)
+                print('まいどあり！ おつりは' + str(charge) + '円です。')
+            else :
+                print('お金が足りません。')
+        else :
+            print(goods_name + 'は取り扱っていません。申し訳ありません。')
 
-    '''
     def set_shop_name(self,shop_name):
-        self.shop_name = shop_name
+        self.__shop_name = shop_name
     
     def get_shop_name(self):
-        return self.shop_name
+        return self.__shop_name
     
     def set_tel_no(self,tel_no):
-        self.tel_no = tel_no
+        self.__tel_no = tel_no
     
     def get_tel_no(self):
-        return self.tel_no
+        return self.__tel_no
     
     shop_name = property(set_shop_name,get_shop_name)
 
     tel_no = property(set_tel_no,get_tel_no)
-    '''
