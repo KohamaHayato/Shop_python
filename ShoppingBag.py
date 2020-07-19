754116dac0377f0ec327f68ebd4aad0d1da81765
KohamaHayato <007.hamako@gmail.com>
2020-07-19 18:01:32 +0900

    S1-4まで完成

--- ShoppingBag.py
+++ ShoppingBag.py
@@ -3,14 +3,15 @@ from Goods import *
 class ShoppingBag:
     def __init__(self,money = 0):
         self.__money = money
-    
+        self.__goods = Goods()
     def print_shopping_bag(self):
-        goods = Goods() #初期化しているので商品が入っていないことになる
+        #初期化しているので商品が入っていないことになる
+        
         print('　(ShoppingBag) 限度額' + str(self.__money) + '円')
-        if goods.get_goods_name() == 'なし':
+        if self.__goods == None:
             print('　(ShoppingBag) 商品は入っていません。')
         else :
-            goods.print_goods()
+            self.__goods.print_goods()
 
     def set_money(self,money):
         self.__money = money
@@ -18,4 +19,12 @@ class ShoppingBag:
     def get_money(self):
         return self.__money
     
+    def set_goods(self,goods):
+        self.__goods = goods
+    
+    def get_goods(self):
+        return self.__goods
+
     money = property(set_money,get_money)
+
+    goods = property(set_goods,get_goods)
